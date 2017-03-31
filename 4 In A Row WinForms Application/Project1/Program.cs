49635c1869efafb560d053d2b490bc36889148b5
Israel Zablianov <123hartk123@gmail.com>
2017-04-01 02:27:44 +0300

    Fixing Bugs

---
+++ 4 In A Row WinForms Application/Project1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace B16_Ex05
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Program
+    {
+        public static void Main() 
+        {
+            GameUI game = new GameUI();
+            game.StartGame();
+        }
+    }
+}
