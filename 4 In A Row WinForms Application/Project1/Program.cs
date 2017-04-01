63614ffc09bc90ede2d2ed941f1b1b3f893f37b8
Israel Zablianov <123hartk123@gmail.com>
2017-04-01 03:34:21 +0300

    adding sulotions

--- 4 In A Row WinForms Application/Project1/Program.cs
+++ 4 In A Row WinForms Application/Project1/Program.cs
@@ -1,12 +1,12 @@
 ﻿namespace B16_Ex05
 {
     using System;
-    using System.Collections.Generic;
+    using System.Collections.Generic; 
     using System.Text;
 
-    public class Program
+    public class Program 
     {
-        public static void Main() 
+        public static void Main()
         {
             GameUI game = new GameUI();
             game.StartGame();
